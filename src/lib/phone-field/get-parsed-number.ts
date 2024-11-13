bc2a6dcb6a6a43896e9d99b7ce138577a3c4ea9a
Zavur Mamashev <43480624+marbleblazer@users.noreply.github.com>
2024-11-13 14:33:11 +0300

    fix: added validation of initial data (#64)
    Co-authored-by: Denis Shtefan <45861949+dshtefan@users.noreply.github.com>

--- src/lib/phone-field/get-parsed-number.ts
+++ src/lib/phone-field/get-parsed-number.ts
@@ -1,15 +1,26 @@
 import { parsePhoneNumber } from 'libphonenumber-js';
 
-export function getParsedNumber(phoneNumberStr: string) {
+export const isCountryCodeValid = (validCountryCodes: string[], countryCode: string) =>
+    validCountryCodes.includes(countryCode);
+
+export function getParsedNumber(validCountryCodes: string[], phoneNumberStr: string) {
+    if (phoneNumberStr.length < 3) {
+        return { countryCode: '', countryCallingCode: '', number: '' };
+    }
+
     // Парсинг номера телефона
     const parsedNumber = parsePhoneNumber(`+${phoneNumberStr}`);
 
     // Получение кода страны
     const countryCode = parsedNumber.country?.toLowerCase();
 
+    if (!isCountryCodeValid(validCountryCodes, countryCode || '')) {
+        return { countryCode: '', countryCallingCode: '1', number: '' };
+    }
+
     const countryCallingCode = parsedNumber.countryCallingCode;
 
-    const number = parsedNumber.formatNational();
+    const number = parsedNumber.nationalNumber;
 
     return { countryCode, countryCallingCode, number };
 }
