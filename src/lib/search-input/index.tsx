87fa7a730bb3f45780bf658f04228f70dd9db070
Zavur Mamashev <43480624+marbleblazer@users.noreply.github.com>
2024-12-03 01:49:42 +0300

    feat: added readOnly to field

--- src/lib/search-input/index.tsx
+++ src/lib/search-input/index.tsx
@@ -30,6 +30,7 @@ export const SearchInput: React.FC<ISearchInputProps> = ({
             isLoading={isLoading}
             InputProps={{
                 value: isLoading ? 'Loading' : value,
+                readOnly: isLoading,
                 onChange: handleInputChange,
                 startAdornment: (
                     <InputAdornment position="start">{isLoading ? <Loader /> : <SearchIcon />}</InputAdornment>
