8b2252386b93fec418d90f2d0e178a301028c269
garaev-insaf <56091490+garaev-insaf@users.noreply.github.com>
2024-12-16 16:58:23 +0300

    feat: dev list items & checbox (#113)
    * feat: dev list items & checbox
    
    * feat: update list item
    
    ---------
    
    Co-authored-by: scotch <igaraev@solit-clouds.ru>
    Co-authored-by: Denis Shtefan <45861949+dshtefan@users.noreply.github.com>

--- src/lib/select-indicator/style.ts
+++ src/lib/select-indicator/style.ts
@@ -5,5 +5,5 @@ export const Root = styled(Box)`
     display: flex;
     align-items: center;
     justify-content: center;
-    color: ${({ theme }) => theme.palette.text.tertiary};
+    color: ${({ theme }) => theme.palette.text.text8};
 `;
