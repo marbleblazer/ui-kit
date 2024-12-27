a7c28982fde66c055b2725b44b13b3a98255f1aa
garaev-insaf <56091490+garaev-insaf@users.noreply.github.com>
2024-12-27 12:59:02 +0300

    fix(button): correct button hover colors (#144)
    Co-authored-by: scotch <igaraev@solit-clouds.ru>
    Co-authored-by: Denis Shtefan <45861949+dshtefan@users.noreply.github.com>

--- src/lib/icon-button/style.ts
+++ src/lib/icon-button/style.ts
@@ -34,7 +34,7 @@ export const ButtonWrapper = styled(IconButton)<IIconButtonProps>(({ theme, size
         color: theme.palette.base.color1,
 
         '&:hover, &:active': {
-            background: theme.palette.base.color6,
+            background: theme.palette.base.hover,
         },
 
         '&:disabled': {
@@ -54,7 +54,7 @@ export const ButtonWrapper = styled(IconButton)<IIconButtonProps>(({ theme, size
 
         '&:hover, &:checked': {
             background: theme.palette.base.color63,
-            borderColor: theme.palette.base.color6,
+            borderColor: theme.palette.base.hover,
         },
 
         '&:active': {
