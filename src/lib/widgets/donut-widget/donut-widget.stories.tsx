7b6ac302c6f3e3001bdfb3d0bc6c69484eb1a157
garaev-insaf <56091490+garaev-insaf@users.noreply.github.com>
2025-01-29 18:38:59 +0300

    feat(widgets): add delete button disabling logic (#186)
    Co-authored-by: scotch <igaraev@solit-clouds.ru>

--- src/lib/widgets/donut-widget/donut-widget.stories.tsx
+++ src/lib/widgets/donut-widget/donut-widget.stories.tsx
@@ -1,8 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { DonutWidget } from '@chirp/ui/lib';
-import { Box, Stack } from '@mui/system';
+import { Box, Stack, useTheme } from '@mui/material';
 import { useState } from 'react';
-import { useTheme } from '@emotion/react';
 
 const meta: Meta<typeof DonutWidget> = {
     title: 'UI/Widgets/DonutWidget',
@@ -19,7 +18,6 @@ type Story = StoryObj<typeof DonutWidget>;
 
 export const Default: Story = {
     render: () => {
-        const theme = useTheme();
         const [favoriteState, setFavoriteState] = useState(false);
         const [deleteState, setDeleteState] = useState(false);
 
@@ -41,7 +39,7 @@ export const Default: Story = {
                         { name: 'Offline', value: 1 },
                     ]}
                     centerText="5/1"
-                    colors={[theme.palette.base.color9, theme.palette.base.color13]}
+                    colors={['#ff00ff', '#ff0000']}
                     type="online"
                     title="Connection state"
                     renderSelectedContent={({ name }) => (
@@ -82,6 +80,54 @@ export const FiveItems: Story = {
                         { name: 'No actual state', value: 0 },
                         { name: 'No coordinates', value: 0 },
                     ]}
+                    colors={[
+                        theme.palette.base.color9,
+                        theme.palette.base.color14,
+                        theme.palette.base.color11,
+                        theme.palette.base.color12,
+                        theme.palette.base.color13,
+                    ]}
+                    type="online"
+                    title="Motion state"
+                    renderSelectedContent={({ name }) => (
+                        <Stack>
+                            any dynamic content
+                            <Box>{name}</Box>
+                        </Stack>
+                    )}
+                />
+            </Box>
+        );
+    },
+};
+
+export const Disabled: Story = {
+    render: () => {
+        const theme = useTheme();
+        const [favoriteState, setFavoriteState] = useState(false);
+        const [deleteState, setDeleteState] = useState(false);
+
+        return (
+            <Box
+                p={5}
+                sx={{
+                    width: '440px',
+                    height: '487px',
+                    background: 'gray',
+                }}
+            >
+                <DonutWidget
+                    isFavorite={favoriteState}
+                    onFavoriteClick={() => setFavoriteState(!favoriteState)}
+                    onDeleteClick={() => setDeleteState(!deleteState)}
+                    data={[
+                        { name: 'Stationary ', value: 5 },
+                        { name: 'Stationary with ignition on', value: 0 },
+                        { name: 'Moving', value: 1 },
+                        { name: 'No actual state', value: 0 },
+                        { name: 'No coordinates', value: 0 },
+                    ]}
+                    deleteDisabled
                     colors={[
                         theme.palette.base.color9,
                         theme.palette.base.color14,
--- src/lib/widgets/list-widget-with-filters/list-widget-with-filters.stories.tsx
+++ src/lib/widgets/list-widget-with-filters/list-widget-with-filters.stories.tsx
@@ -1,8 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { ListWidgetWithFilters, SearchInput, Select, Typography } from '@chirp/ui/lib';
-import { Box, Stack, MenuItem } from '@mui/material';
+import { Box, Stack, MenuItem, useTheme } from '@mui/material';
 import { Fragment, useState } from 'react';
-import { useTheme } from '@emotion/react';
 import * as S from './styles';
 
 const meta: Meta<typeof ListWidgetWithFilters> = {
@@ -66,6 +65,113 @@ export const Default: Story = {
                     onDeleteClick={() => setDeleteState(!deleteState)}
                     data={mockedData}
                     nameKey="name"
+                    valueKey="value"
+                    renderFilters={
+                        <Stack direction="row" gap={2}>
+                            <SearchInput
+                                sx={{
+                                    backgroundColor: theme.palette.background.background2,
+                                }}
+                                placeholder="Search by name"
+                                value=""
+                                onChange={() => {}}
+                            />
+                            <Select
+                                placeholder="All"
+                                sx={{
+                                    '&.MuiInputBase-root .MuiSelect-select': {
+                                        py: 1.25,
+                                        pl: 3,
+                                    },
+                                    height: 28,
+                                    mt: 0,
+                                    border: 'none',
+                                    backgroundColor: theme.palette.background.background2,
+                                }}
+                                fullWidth
+                                error={false}
+                            >
+                                <MenuItem sx={{ height: 20 }} value={1}>
+                                    Exceeding 1 - 10 km/h
+                                </MenuItem>
+                                <MenuItem sx={{ height: 20 }} value={2}>
+                                    Exceeding 1 - 10 km/h
+                                </MenuItem>
+                                <MenuItem sx={{ height: 20 }} value={3}>
+                                    Exceeding 1 - 10 km/h
+                                </MenuItem>
+                                <MenuItem sx={{ height: 20 }} value={4}>
+                                    Exceeding 1 - 10 km/h
+                                </MenuItem>
+                            </Select>
+                        </Stack>
+                    }
+                    type="period"
+                    renderSelectedContent={(item) => (
+                        <S.ListWrapper>
+                            {['Exceeding', 'Date'].map((name) => (
+                                <S.HeaderWrapper>
+                                    <Typography
+                                        sx={{
+                                            color: 'text.text8',
+                                        }}
+                                        variant="overline"
+                                    >
+                                        {name}
+                                    </Typography>
+                                </S.HeaderWrapper>
+                            ))}
+                            {mockedData.map((_, index) => (
+                                <Fragment key={index}>
+                                    <Typography
+                                        sx={{
+                                            color: 'text.text1',
+                                        }}
+                                        variant="caption12"
+                                    >
+                                        {item.name}
+                                    </Typography>
+                                    <Typography
+                                        sx={{
+                                            color: 'text.text1',
+                                        }}
+                                        variant="caption12"
+                                        textAlign="end"
+                                    >
+                                        {new Date().toISOString().split('T')[0]}
+                                    </Typography>
+                                </Fragment>
+                            ))}
+                        </S.ListWrapper>
+                    )}
+                    title="Speed violations"
+                />
+            </Box>
+        );
+    },
+};
+export const Disabled: Story = {
+    render: () => {
+        const theme = useTheme();
+        const [favoriteState, setFavoriteState] = useState(false);
+        const [deleteState, setDeleteState] = useState(false);
+
+        return (
+            <Box
+                p={5}
+                sx={{
+                    width: '762px',
+                    height: '343px',
+                    background: 'gray',
+                }}
+            >
+                <ListWidgetWithFilters
+                    isFavorite={favoriteState}
+                    onFavoriteClick={() => setFavoriteState(!favoriteState)}
+                    onDeleteClick={() => setDeleteState(!deleteState)}
+                    data={mockedData}
+                    nameKey="name"
+                    deleteDisabled
                     valueKey="value"
                     renderFilters={
                         <Stack direction="row" gap={2}>
