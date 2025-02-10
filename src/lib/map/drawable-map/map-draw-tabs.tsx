8d21b358e6360bffc65c14f6046b0b0de9fe4aff
garaev-insaf <56091490+garaev-insaf@users.noreply.github.com>
2025-02-10 18:37:21 +0300

    feat: add trasnaltion in stacked-line-chart-wdiget and map tabs (#207)
    Co-authored-by: scotch <igaraev@solit-clouds.ru>
    Co-authored-by: Denis Shtefan <45861949+dshtefan@users.noreply.github.com>

--- src/lib/map/drawable-map/map-draw-tabs.tsx
+++ src/lib/map/drawable-map/map-draw-tabs.tsx
@@ -1,27 +1,32 @@
-// import mapboxgl from 'mapbox-gl';
-
 import * as S from '../style';
 import { Tabs } from '../../tabs';
+import { useTranslation } from 'react-i18next';
 
 interface IMapDrawModeTabsProps {
     activeMode?: string;
     onChangeMode: (mode: string) => void;
 }
 
-const drawModes = [
+type DrawModesType = {
+    key: 'draw_line_string' | 'draw_circle' | 'draw_polygon';
+    title: 'Line' | 'Circle' | 'Polygon';
+};
+
+const drawModes: DrawModesType[] = [
     { key: 'draw_line_string', title: 'Line' },
     { key: 'draw_circle', title: 'Circle' },
     { key: 'draw_polygon', title: 'Polygon' },
 ];
 
 export const MapDrawModeTabs: React.FC<IMapDrawModeTabsProps> = ({ activeMode, onChangeMode }) => {
+    const { t } = useTranslation('uiKit', { keyPrefix: 'map' });
     const resolvedKey = drawModes.find((node) => node.key === activeMode)?.key;
 
     return (
         <S.MapDrawModeTabsWrapper>
             <Tabs
                 items={drawModes}
-                resolvedTitle={(node) => node.title}
+                resolvedTitle={(node) => t(node.title)}
                 resolvedValue={(node) => node.key}
                 activeTab={resolvedKey || false}
                 setActiveTab={onChangeMode}
