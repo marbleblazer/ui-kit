5082cf3be3d9653b409e919b5abd4573a2d9574d
Zavur Mamashev <43480624+marbleblazer@users.noreply.github.com>
2025-02-26 17:36:48 +0300

    feat: added props for user popup, changed styles of avatar

--- src/lib/avatar/styles.tsx
+++ src/lib/avatar/styles.tsx
@@ -2,7 +2,5 @@ import { Avatar, styled } from '@mui/material';
 
 export const StyledAvatar = styled(Avatar)(({ theme }) => ({
     color: theme.palette.text.text8,
-    backgroundColor: theme.palette.background.background5,
-    border: '1px solid',
-    borderColor: theme.palette.border.border4,
+    backgroundColor: theme.palette.text.titleInput,
 }));
