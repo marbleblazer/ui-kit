f6d0a42a5c58f7ef52d3fceae7c9ac6f6e5fbf82
garaev-insaf <56091490+garaev-insaf@users.noreply.github.com>
2025-03-13 17:52:24 +0300

    feat: add isLoading prop and dev this logic (#252)
    Co-authored-by: Denis Shtefan <45861949+dshtefan@users.noreply.github.com>

--- src/lib/button/index.tsx
+++ src/lib/button/index.tsx
@@ -1,9 +1,29 @@
 import { FC } from 'react';
 import * as S from './style';
 import { IButtonProps } from './types';
+import { Loader } from '../loader';
+import { Box, useTheme } from '@mui/material';
 
-export const Button: FC<IButtonProps> = ({ children, ...props }) => (
-    <S.ButtonWrapper {...props} disableRipple>
-        {children}
-    </S.ButtonWrapper>
-);
+export const Button: FC<IButtonProps> = ({ children, isLoading, variant, disabled, ...props }) => {
+    const theme = useTheme();
+
+    return (
+        <Box position="relative">
+            {isLoading && (
+                <S.LoaderWrapper>
+                    <Loader size="small" color={theme.palette.base.color1} />
+                </S.LoaderWrapper>
+            )}
+            <S.ButtonWrapper
+                {...props}
+                variant={variant}
+                isLoading={isLoading}
+                disabled={isLoading || disabled}
+                disableRipple
+            >
+                {isLoading && <S.Backdrop variant={variant} />}
+                {children}
+            </S.ButtonWrapper>
+        </Box>
+    );
+};
--- src/lib/icon-button/index.tsx
+++ src/lib/icon-button/index.tsx
@@ -1,8 +1,28 @@
+import { Box, useTheme } from '@mui/material';
 import * as S from './style';
 import { IIconButtonProps } from './types';
+import { Loader } from '../loader';
 
-export const IconButton: React.FC<IIconButtonProps> = ({ children, ...props }) => (
-    <S.ButtonWrapper {...props} disableRipple>
-        {children}
-    </S.ButtonWrapper>
-);
+export const IconButton: React.FC<IIconButtonProps> = ({ children, isLoading, variant, disabled, ...props }) => {
+    const theme = useTheme();
+
+    return (
+        <Box position="relative">
+            {isLoading && (
+                <S.LoaderWrapper>
+                    <Loader size="small" color={theme.palette.base.color1} />
+                </S.LoaderWrapper>
+            )}
+            <S.ButtonWrapper
+                {...props}
+                variant={variant}
+                isLoading={isLoading}
+                disabled={isLoading || disabled}
+                disableRipple
+            >
+                {isLoading && <S.Backdrop variant={variant} />}
+                {children}
+            </S.ButtonWrapper>
+        </Box>
+    );
+};
