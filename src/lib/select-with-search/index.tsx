a0c7a29846a2a9bb60d571d3e0893fea4787bcea
scotch <insafgaraev47@gmail.com>
2025-04-23 16:45:28 +0300

    feat: init and dev select with search

---
+++ src/lib/select-with-search/index.tsx
@@ -0,0 +1,132 @@
+import { FormControl, InputAdornment, MenuItem, SelectProps, useTheme } from '@mui/material';
+import { FC, useMemo, useState } from 'react';
+
+import { InputLabel } from '../input-label';
+import * as S from './style';
+import { CloseIcon } from '@chirp/ui/assets/icons';
+import { IconButton } from '../icon-button';
+import { TextField } from '../text-field';
+import { SearchIcon } from '@chirp/ui/assets/fleet-icons';
+
+export type SelectWithSearchOptionType = {
+    value: string | number;
+    name: string | number;
+    badge?: string;
+};
+
+export type SelectWithSearchPropsType = Omit<SelectProps, 'onClose' | 'variant' | 'collection'> & {
+    onClear?: () => void;
+    searchPlaceholder?: string;
+    collection: SelectWithSearchOptionType[];
+};
+
+export const SelectWithSearch: FC<SelectWithSearchPropsType> = ({
+    label,
+    labelId,
+    disabled,
+    value,
+    onClear,
+    collection = [],
+    searchPlaceholder,
+    endAdornment,
+    MenuProps,
+    ...props
+}) => {
+    const theme = useTheme();
+    const [searchText, setSearchText] = useState('');
+
+    const mergedMenuProps = {
+        ...MenuProps,
+        MenuListProps: {
+            ...MenuProps?.MenuListProps,
+            sx: {
+                padding: 0,
+                ...MenuProps?.MenuListProps?.sx,
+            },
+        },
+    };
+
+    const filteredCollection = useMemo(() => {
+        if (searchText?.length) {
+            return collection.filter((item) => String(item.name).toLowerCase().includes(searchText.toLowerCase()));
+        }
+
+        return collection;
+    }, [searchText, collection]);
+
+    return (
+        <FormControl fullWidth>
+            <InputLabel
+                labelId={labelId}
+                label={label}
+                className={props.error ? 'Mui-error' : undefined}
+                sx={{
+                    opacity: disabled ? 0.3 : 1,
+                    '&.MuiInputLabel-shrink': {
+                        transform: 'none',
+                        left: 0,
+                        color: theme.palette.text.titleInput,
+                        ...theme.typography.caption12,
+                    },
+                }}
+            />
+            <S.Select
+                {...props}
+                value={value}
+                disabled={disabled}
+                MenuProps={mergedMenuProps}
+                onClose={() => setSearchText('')}
+                endAdornment={
+                    endAdornment ? (
+                        endAdornment
+                    ) : onClear && value ? (
+                        <InputAdornment sx={{ marginRight: '16px' }} position="end">
+                            <IconButton variant="gray" onClick={onClear}>
+                                <CloseIcon />
+                            </IconButton>
+                        </InputAdornment>
+                    ) : null
+                }
+            >
+                <S.ListSubheader onClick={(e) => e.stopPropagation()}>
+                    <TextField
+                        size="small"
+                        value={searchText}
+                        // autoFocus
+                        placeholder={searchPlaceholder}
+                        fullWidth
+                        onClick={(e) => e.stopPropagation()}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <SearchIcon />
+                                </InputAdornment>
+                            ),
+                        }}
+                        onChange={(e) => setSearchText(e.target.value)}
+                        onKeyDown={(e) => {
+                            if (e.key !== 'Escape') {
+                                e.stopPropagation();
+                            }
+                        }}
+                    />
+                </S.ListSubheader>
+                {filteredCollection.length
+                    ? filteredCollection.map(({ badge, ...rest }) => (
+                          <MenuItem key={rest.name} value={String(rest.value)}>
+                              <S.MenuItemWrapper
+                                  width="100%"
+                                  direction="row"
+                                  justifyContent="space-between"
+                                  alignItems="center"
+                              >
+                                  {String(rest.name)}
+                                  {badge ? <span>{badge}</span> : null}
+                              </S.MenuItemWrapper>
+                          </MenuItem>
+                      ))
+                    : null}
+            </S.Select>
+        </FormControl>
+    );
+};
---
+++ src/lib/select-with-search/style.ts
@@ -0,0 +1,103 @@
+import { alpha, ListSubheader as MUIListSubheader, SelectProps, Stack, styled } from '@mui/material';
+import { Select as MuiSelect } from '@mui/material';
+
+export const Select = styled(MuiSelect)<SelectProps>(({ placeholder, theme }) => ({
+    backgroundColor: theme.palette.background.background2,
+    borderRadius: '8px',
+    border: '1px solid',
+    borderColor: alpha(theme.palette.border.input, 0.14),
+    ...theme.typography.body1,
+    marginTop: '20px',
+
+    '&:hover:not(.Mui-disabled)': {
+        backgroundColor: alpha(theme.palette.background.background11, 0.5),
+        borderColor: alpha(theme.palette.border.input2, 0.24),
+        '.MuiSelect-icon': {
+            color: alpha(theme.palette.text.textInput80, 0.8),
+        },
+    },
+
+    '&.Mui-disabled': {
+        opacity: '0.3 !important',
+        '& + label': {
+            opacity: '0.3 !important',
+        },
+    },
+
+    '.MuiSelect-select': {
+        paddingTop: '12px',
+        paddingLeft: '16px',
+        paddingBottom: '8px',
+        ...theme.typography.body1,
+
+        '&.MuiInputBase-input': {
+            paddingTop: '10px',
+            paddingLeft: '16px',
+            paddingRight: '36px',
+            paddingBottom: '10px',
+            fontSize: '13px',
+            lineHeight: '20px',
+        },
+    },
+
+    '& .MuiSelect-select .notranslate::after': placeholder
+        ? {
+              content: `"${placeholder}"`,
+              color: alpha(theme.palette.text.textInput60, 0.6),
+          }
+        : {},
+
+    '&:hover::before': placeholder
+        ? {
+              color: alpha(theme.palette.text.textInput80, 0.8),
+          }
+        : {},
+
+    '.MuiSelect-icon': {
+        color: alpha(theme.palette.text.textInput60, 0.6),
+        right: '16px',
+
+        '&:not(:empty)': {
+            color: theme.palette.text.text1,
+        },
+    },
+
+    '.MuiInputBase-adornedEnd': {
+        paddingRight: '20px',
+    },
+
+    input: {
+        padding: '12px 16px 8px 16px',
+        fontSize: '13px',
+        lineHeight: '20px',
+        ...theme.typography.body1,
+    },
+    fieldset: {
+        border: 'none',
+        top: 0,
+    },
+    legend: {
+        ...theme.typography.body1,
+        backgroundColor: 'transparent',
+        span: {
+            padding: 0,
+        },
+    },
+}));
+
+export const ListSubheader = styled(MUIListSubheader)(({ theme }) => ({
+    padding: '16px',
+    background: theme.palette.background.background1,
+
+    '.MuiFormControl-root': {
+        marginTop: 0,
+    },
+}));
+
+export const MenuItemWrapper = styled(Stack)(({ theme }) => ({
+    display: 'flex',
+    justifyContent: 'space-between',
+    span: {
+        color: theme.palette.base.color6,
+    },
+}));
