929982f79c43acc80b3df34c3632ec960ba18af8
scotch <insafgaraev47@gmail.com>
2025-06-19 17:52:43 +0300

    feat: dev toasts stacking and close icon

--- src/lib/toast-container/index.tsx
+++ src/lib/toast-container/index.tsx
@@ -1,14 +1,29 @@
+import { CloseIcon } from '@chirp/ui/assets/icons';
 import * as S from './style';
+import Box from '@mui/material/Box';
 
 export const ToastContainer = () => {
     return (
         <S.ToastContainer
-            autoClose={2000}
-            closeButton={false}
+            autoClose={20000}
             closeOnClick={false}
             icon={false}
             hideProgressBar
-            limit={5}
+            stacked
+            closeButton={({ ...props }) => (
+                <Box
+                    sx={{
+                        position: 'absolute',
+                        top: 4,
+                        right: 4,
+                        cursor: 'pointer',
+                    }}
+                    {...props}
+                    onClick={props.closeToast}
+                >
+                    <CloseIcon />
+                </Box>
+            )}
             position="bottom-right"
         />
     );
--- src/lib/toast-container/style.ts
+++ src/lib/toast-container/style.ts
@@ -1,8 +1,21 @@
+import { CloseIcon } from '@chirp/ui/assets/icons';
 import { styled } from '@mui/material';
 import { ToastContainer as RTToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 
 export const ToastContainer = styled(RTToastContainer)`
+    .Toastify__toast--stacked[data-collapsed='true']:not(:last-child) > * {
+        opacity: 0.1;
+    }
+    .Toastify__toast--stacked {
+        &::after {
+            display: none !important;
+        }
+    }
+    .Toastify__progress-bar--wrp {
+        display: none !important;
+    }
+
     &&&.Toastify__toast-container {
         background: transparent;
         width: min(400px, calc(100% - 2rem));
@@ -20,3 +33,10 @@ export const ToastContainer = styled(RTToastContainer)`
         padding: 0;
     }
 `;
+
+export const StyledCrossHairIcon = styled(CloseIcon)({
+    position: 'absolute',
+    top: 4,
+    right: 4,
+    cursor: 'pointer',
+});
