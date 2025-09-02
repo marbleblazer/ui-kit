fa083f068aeb0c06d1fca2aa8903b7f84bef64c3
Zavur Mamashev <43480624+marbleblazer@users.noreply.github.com>
2025-09-02 12:01:55 +0300

    feat: added language detector

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -3,22 +3,25 @@ import { initReactI18next } from 'react-i18next';
 
 import { languages } from './languages';
 import resources from './resources';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 const LANGUAGE_COOKIE_KEY = 'language';
 
-i18n.use(initReactI18next).init({
-    resources,
-    fallbackLng: 'ru',
-    supportedLngs: Object.keys(languages),
-    interpolation: {
-        escapeValue: false,
-    },
-    detection: {
-        order: ['cookie'],
-        lookupCookie: LANGUAGE_COOKIE_KEY,
-        caches: ['cookie'],
-    },
-});
+i18n.use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources,
+        fallbackLng: 'en',
+        supportedLngs: Object.keys(languages),
+        interpolation: {
+            escapeValue: false,
+        },
+        detection: {
+            order: ['cookie'],
+            lookupCookie: LANGUAGE_COOKIE_KEY,
+            caches: ['cookie'],
+        },
+    });
 
 export const changeLanguage = (lang: string) => {
     i18n.changeLanguage(lang);
